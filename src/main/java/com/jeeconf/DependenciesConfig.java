02adc7a2874a3283c5c9bdf151d9d7a505aeaab9
sshogunn <sshogunn@gmail.com>
2017-05-26 06:39:34 +0300

    Registration by type of class

--- src/main/java/com/jeeconf/DependenciesConfig.java
+++ src/main/java/com/jeeconf/DependenciesConfig.java
@@ -1,15 +1,15 @@
 package com.jeeconf;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 class DependenciesConfig {
-    private List<Object> regInstances = new ArrayList<>();
+    private Map<Class<?>, Object> regInstances = new HashMap<>();
 
-    void register(Class<?> regClass) {
+    Registration register(Class<?> regClass) {
         Object instance = loadObject(regClass);
-        this.regInstances.add(instance);
+        return new Registration(instance);
     }
 
     private Object loadObject(Class<?> regClass) {
@@ -22,6 +22,26 @@ class DependenciesConfig {
 
     @SuppressWarnings("unchecked")
     <T> T findInstance(Class<T> type) {
-        return (T) regInstances.stream().filter(i -> Objects.equals(i.getClass(), type)).findFirst().get();
+        Class<?> key = regInstances.keySet().stream().filter(i -> Objects.equals(i, type)).findFirst().get();
+        return (T) regInstances.get(key);
+    }
+
+    public class Registration {
+        private Object instance;
+        private Class<?> type;
+
+        Registration(Object instance) {
+            this.instance = instance;
+            this.type = instance.getClass();
+        }
+
+        Registration as(Class<?> type) {
+            this.type = type;
+            return this;
+        }
+
+        void complete() {
+            regInstances.put(type, instance);
+        }
     }
 }
--- src/test/java/com/jeeconf/BeansRegistrationTest.java
+++ src/test/java/com/jeeconf/BeansRegistrationTest.java
@@ -1,11 +1,12 @@
 package com.jeeconf;
 
+import com.jeeconf.testing.simple.Presentation;
 import com.jeeconf.testing.simple.Training;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class BeansRegistrationTest {
 
@@ -13,11 +14,23 @@ public class BeansRegistrationTest {
     public void shouldReturnRegisteredBean() {
         //GIVEN
         DependenciesConfig config = new DependenciesConfig();
-        config.register(Training.class);
+        config.register(Training.class).complete();
         JEEConfInjector injector = new JEEConfInjector(config);
         //WHEN
         Training training = injector.get(Training.class);
         //THEN
         assertThat(training, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldReturnRegisteredByTypeBean() {
+        //GIVEN
+        DependenciesConfig config = new DependenciesConfig();
+        config.register(Training.class).as(Presentation.class).complete();
+        JEEConfInjector injector = new JEEConfInjector(config);
+        //WHEN
+        Presentation training = injector.get(Presentation.class);
+        //THEN
+        assertThat(training, is(notNullValue()));//
     }
 }
